c92983572376ef27a19d964aac626af408d65024
Vimukthanjani Anumala <vimuktha@Vimukthanjanis-MacBook-Air.local>
2026-01-30 16:27:32 +0530

    Reset and fix api migrations

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -24,7 +24,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('server.api.urls')),
+    path('api/', include('api.urls')),
 
     # Auth
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
